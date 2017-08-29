87d43ef56f14af479afc0e792514a3f4a68b43b5
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2017-08-29 10:34:28 +0000

    29/09/2017

--- src/main/java/istat/android/widgets/list/adapters/abs/AbsAdapter.java
+++ src/main/java/istat/android/widgets/list/adapters/abs/AbsAdapter.java
@@ -1,8 +1,6 @@
 package istat.android.widgets.list.adapters.abs;
 
-import istat.android.base.image.ImageLoader;
-import istat.android.base.image.ImageLoader.ImageLoadListener;
-import istat.android.base.image.ImageLoader.PhotoToLoad;
+import istat.android.base.utils.ImageLoader;
 import istat.android.widgets.list.adapters.utils.ViewNotSupportedException;
 
 import java.util.List;
@@ -18,122 +16,120 @@ import android.widget.ImageView;
 import android.widget.SimpleAdapter.ViewBinder;
 
 /**
- * 
- * @author Toukea tatsi (Istat)
- * 
  * @param <viewValue>
+ * @author Toukea tatsi (Istat)
  */
 public abstract class AbsAdapter<viewValue> extends
-		ArrayAdapter<Map<Integer, viewValue>> {
+        ArrayAdapter<Map<Integer, viewValue>> {
 
-	ImageLoader imageLoader;
-	protected LayoutInflater inflater;
-	protected int baseLayoutRessource = 0;
+    ImageLoader imageLoader;
+    protected LayoutInflater inflater;
+    protected int baseLayoutResource = 0;
 
-	// protected List<Map<Integer,viewValue>> listItems=new
-	// ArrayList<Map<Integer,viewValue>>();
+    // protected List<Map<Integer,viewValue>> listItems=new
+    // ArrayList<Map<Integer,viewValue>>();
 
-	public AbsAdapter(Context context, int textViewResourceId,
+    public AbsAdapter(Context context, int textViewResourceId,
                       List<Map<Integer, viewValue>> objects) {
-		super(context, textViewResourceId, objects);
-		// listItems=objects;
-		baseLayoutRessource = textViewResourceId;
-		imageLoader = new ImageLoader(context);
-		inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		imageLoader.setLoadListener(imageLoadListener);
+        super(context, textViewResourceId, objects);
+        // listItems=objects;
+        baseLayoutResource = textViewResourceId;
+        imageLoader = new ImageLoader(context);
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        imageLoader.setLoadListener(imageLoadListener);
 
-	}
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		View rootView = null;
-		if (convertView != null)
-			rootView = convertView;
-		else {
-			rootView = inflater.inflate(baseLayoutRessource, parent, false);
-		}
-		Map<Integer, ?> item = getItem(position);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View rootView = null;
+        if (convertView != null)
+            rootView = convertView;
+        else {
+            rootView = inflater.inflate(baseLayoutResource, parent, false);
+        }
+        Map<Integer, ?> item = getItem(position);
 
-		for (int i : item.keySet()) {
-			View tmpView = rootView.findViewById(i);
-			if (tmpView != null) {
-				if (viewBinder.setViewValue(tmpView, item.get(i), item.get(i)
-						.toString()))
-					;
-				else if (defaultViewBinder.setViewValue(tmpView, item.get(i),
-						item.get(i).toString()))
-					;
-				else
-					throw new ViewNotSupportedException(
-							tmpView.getClass().toString()
-									+ " is not a view that can be bound by AbsAdapter");
+        for (int i : item.keySet()) {
+            View tmpView = rootView.findViewById(i);
+            if (tmpView != null) {
+                if (viewBinder.setViewValue(tmpView, item.get(i), item.get(i)
+                        .toString()))
+                    ;
+                else if (defaultViewBinder.setViewValue(tmpView, item.get(i),
+                        item.get(i).toString()))
+                    ;
+                else
+                    throw new ViewNotSupportedException(
+                            tmpView.getClass().toString()
+                                    + " is not a view that can be bound by AbsAdapter");
 
-			}
-		}
+            }
+        }
 
-		return rootView;
-	}
+        return rootView;
+    }
 
-	void setViewImage(final ImageView v, final String path) {
-		imageLoader.DisplayImage(path, v);
-	}
+    void setViewImage(final ImageView v, final String path) {
+        imageLoader.displayImage(path, v);
+    }
 
-	public void setViewBinder(ViewBinder viewBinder) {
-		this.viewBinder = viewBinder;
-	}
+    public void setViewBinder(ViewBinder viewBinder) {
+        this.viewBinder = viewBinder;
+    }
 
-	public void setDefaultViewBinder() {
-		viewBinder = defaultViewBinder;
-	}
+    public void setDefaultViewBinder() {
+        viewBinder = defaultViewBinder;
+    }
 
-	public ImageLoader getImageLoader() {
-		return imageLoader;
-	}
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
 
-	public static ArrayAdapter<String> getArrayAdapter(Context context,
-			List<String> list, int choice) {
+    public static ArrayAdapter<String> getArrayAdapter(Context context,
+                                                       List<String> list, int choice) {
 
-		return new ArrayAdapter<String>(context, choice, list);
+        return new ArrayAdapter<String>(context, choice, list);
 
-	}
+    }
 
-	public static ArrayAdapter<String> getArrayAdapter(Context context,
-			String array[], int choice) {
+    public static ArrayAdapter<String> getArrayAdapter(Context context,
+                                                       String array[], int choice) {
 
-		return new ArrayAdapter<String>(context, choice, array);
+        return new ArrayAdapter<String>(context, choice, array);
 
-	}
+    }
 
-	private ViewBinder defaultViewBinder = new ViewBinder() {
-		private AbsAdapterViewBinderViewValueSeter viewBinderManager = AbsAdapterViewBinderViewValueSeter
-				.getInstance(AbsAdapter.this);
+    private ViewBinder defaultViewBinder = new ViewBinder() {
+        private AbsAdapterViewBinderViewValueSeter viewBinderManager = AbsAdapterViewBinderViewValueSeter
+                .getInstance(AbsAdapter.this);
 
-		@Override
-		public boolean setViewValue(View tmpView, Object item,
-				String textRepresentation) {
-			// TODO Auto-generated method stub
-			return viewBinderManager
-					.bindView(tmpView, item, textRepresentation);
-		}
+        @Override
+        public boolean setViewValue(View tmpView, Object item,
+                                    String textRepresentation) {
+            // TODO Auto-generated method stub
+            return viewBinderManager
+                    .bindView(tmpView, item, textRepresentation);
+        }
 
-	};
-	private ImageLoadListener imageLoadListener = new ImageLoadListener() {
+    };
+    private ImageLoader.ImageLoadListener imageLoadListener = new ImageLoader.ImageLoadListener() {
 
-		@Override
-		public boolean onLoadFinish(PhotoToLoad phLoad, Bitmap bitmap) {
-			// TODO Auto-generated method stub
-			notifyDataSetChanged();
-			return false;
-		}
+        @Override
+        public boolean onLoadFinish(ImageLoader.PhotoToLoad phLoad, Bitmap bitmap) {
+            // TODO Auto-generated method stub
+            notifyDataSetChanged();
+            return false;
+        }
 
-		@Override
-		public boolean onLoad(PhotoToLoad phLoad) {
-			// TODO Auto-generated method stub
+        @Override
+        public boolean onLoad(ImageLoader.PhotoToLoad phLoad) {
+            // TODO Auto-generated method stub
 
-			return false;
-		}
-	};
-	private ViewBinder viewBinder = defaultViewBinder;
+            return false;
+        }
+    };
+    private ViewBinder viewBinder = defaultViewBinder;
 }
